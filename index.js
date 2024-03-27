8428beacfffaee90fd2f1a0e8689f0462fb8b8ba
Ceusesco <ceusescon@gmail.com>
2024-03-27 19:27:11 +0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code in this file!
+// // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// //Welcome massage
+ let welcomeMessage ='Welcome to Flatbook, !'+ currentUser + '!';
+
+// Excited welcome message.
+// let excitedWelcomeMessage ='WELCOME TO FLATBOOK, !' ;
+//Fixing the    1) contains the value of the 'currentUser' variable bug.
+//The value is eaqual to GRACE HOPPER
+
+let excitedWelcomeMessage = 'GRACE HOPPER'+ 'WELCOME TO FLATBOOK, !' ;
+
+// shortGreeting
+let shortGreeting ='Welcome, '+'GRACE!';
